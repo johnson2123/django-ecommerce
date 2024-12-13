97c09c46c3b58b004efbb04525c34d342827ebfd
Johnson <johnsonvasupalli@yahoo.com>
2024-12-14 03:09:52 +0530

    final 6

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = 'django-insecure-4^z=#4&44lf6x8g%191ujn49@ak37r5i#j9+&p@$dtssk6zssj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-ecommerce-app-production-ba08.up.railway.app', 'https://django-ecommerce-app-production-ba08.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://django-ecommerce-app-production-ba08.up.railway.app']
+ALLOWED_HOSTS = ['https://django-ecommerce-app-production-f102.up.railway.app', 'django-ecommerce-app-production-f102.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://django-ecommerce-app-production-f102.up.railway.app']
 
 
 # Application definition
@@ -96,8 +96,8 @@ DATABASES = {
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': DB_PASS_YO,
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '26946',
 
     }
 }
