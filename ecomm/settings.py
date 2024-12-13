1315ec95eb1f3b75be58eaacea90f267458bf45f
Johnson <johnsonvasupalli@yahoo.com>
2024-12-14 02:40:14 +0530

    final 5

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-from django.conf.global_settings import STATICFILES_DIRS, CSRF_TRUSTED_ORIGINS
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
