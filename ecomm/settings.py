e3f859eca48872b2b9c1438a5ade79d67159c965
Johnson <johnsonvasupalli@yahoo.com>
2024-12-17 01:59:06 +0530

    password added

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'oleJovtLvpumCjwZlXboBlJTfkoCnlaG',
+        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
         'HOST': 'autorack.proxy.rlwy.net',
         'PORT': '57263',
 
