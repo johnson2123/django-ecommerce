c2976d9fdbac9dd56a31e35cb0aa97c93281b4c9
Johnson <johnsonvasupalli@yahoo.com>
2024-12-17 21:59:51 +0530

    deploy

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-4^z=#4&44lf6x8g%191ujn49@ak37r5i#j9+&p@$dtssk6zssj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = []
 
 
